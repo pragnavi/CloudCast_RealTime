116e101fed249aea98319279e342f1620148a6a2
pragnavi <pragnavi1398@gmail.com>
2024-03-12 21:54:21 -0400

    Added Network Requests - Display Real-Time Data

--- CloudCast/CurrentWeatherForecast.swift
+++ CloudCast/CurrentWeatherForecast.swift
@@ -8,6 +8,30 @@
 import Foundation
 import UIKit
 
+struct WeatherAPIResponse: Decodable {
+  let currentWeather: CurrentWeatherForecast
+
+  private enum CodingKeys: String, CodingKey {
+    case currentWeather = "current_weather"
+  }
+}
+
+struct CurrentWeatherForecast: Decodable {
+  let windSpeed: Double
+  let windDirection: Double
+  let temperature: Double
+  let weatherCodeRaw: Int
+  var weatherCode: WeatherCode {
+    return WeatherCode(rawValue: weatherCodeRaw) ?? .clearSky
+  }
+  private enum CodingKeys: String, CodingKey {
+    case windSpeed = "windspeed"
+    case windDirection = "winddirection"
+    case temperature = "temperature"
+    case weatherCodeRaw = "weathercode"
+  }
+}
+
 // Based on https://open-meteo.com/en/docs
 enum WeatherCode: Int {
   case clearSky = 0
